50f935bf451cf9cc6a961daf7695aa32b314e517
dyrehauge <dthomasn@gmail.com>
2014-10-10 20:53:24 +0200

    deleted double on script.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -33,43 +33,6 @@ $(function() {
         return false;
     });
     
-    // google maps
-    var map;
-
-    function initialize() {
-
-        var mapCanvas = document.getElementById('map_canvas');
-        var mapOptions = {
-            center: new google.maps.LatLng(55.3809154, 10.4086351),
-            zoom: 15,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            disableDefaultUI: true,
-            draggable: false,
-            zoomControl: false,
-            scrollwheel: false,
-            disableDoubleClickZoom: true
-        };
-
-        map = new google.maps.Map(mapCanvas, mapOptions);
-
-        // google maps color change
-        map.set('styles', [
-         {
-          featureType: 'all',
-          elementType: 'all',
-          stylers: [
-            { saturation: -100 } // <-- THIS
-          ]
-        }
-        ]);
-
-        /* jshint unused:false */
-        var marker = new google.maps.Marker({
-          position: new google.maps.LatLng(55.3809154, 10.4086351),
-          map: map,
-          icon: '../img/googlepointer.png',
-          size: new google.maps.Size(10, 10)
-
 
     // slide effect on shoppingcart click
     $('li.shop').on('click', function() {
